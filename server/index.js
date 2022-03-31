fc60f5ecd8a32e8d10be6fd76dc536480779d497
coding-to-music <connors.tom@gmail.com>
2022-03-31 04:49:55 +0000

    Heroku

--- server/index.js
+++ server/index.js
@@ -16,6 +16,17 @@ const port = process.env.PORT || 4000;
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
+// Answer API requests.
+app.get("/api", function (req, res) {
+  res.set("Content-Type", "application/json");
+  res.send('{"message":"Hello from the custom server!"}');
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
 // middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
